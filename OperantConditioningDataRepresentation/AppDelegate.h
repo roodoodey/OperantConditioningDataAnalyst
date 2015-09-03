b6100a2718b5196a7f883e4c6cdabe4a310ee23c
roodoodey <mathieugrettir@hotmail.com>
2015-09-03 02:06:31 +0000

    Initial Commit

---
+++ OperantConditioningDataRepresentation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OperantConditioningDataRepresentation
+//
+//  Created by Mathieu Skulason on 03/09/15.
+//  Copyright (c) 2015 Mathieu Skulason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OperantConditioningDataRepresentation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OperantConditioningDataRepresentation
+//
+//  Created by Mathieu Skulason on 03/09/15.
+//  Copyright (c) 2015 Mathieu Skulason. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
